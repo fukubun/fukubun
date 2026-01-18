22e5b8217f2f7bc25fc36fccb4c98447af788fdc
fukubun <fukubun.project@gmail.com>
2026-01-19 02:02:59 +0900

    mongo atlas

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/fukubun')
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
